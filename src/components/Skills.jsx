43a2177cf64e8d6f6a7989b5e369d16add8d6dee
Raha <f.keshir25@gmail.com>
2022-01-15 19:44:15 +0330

    add sass folder

---
+++ src/components/Skills.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Skills = () => {
+    return (
+        <div className='home'>
+            <div className='container'>
+                <h2>Skills</h2>
+            </div>
+        </div>
+    )
+}
+export default Skills;
